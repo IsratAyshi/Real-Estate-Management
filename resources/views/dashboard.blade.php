ec8abb34798031e1ed581c9b9a0f662160318b14
Israt Jahan Ayshi <israt.ayshi1999@gmail.com>
2024-03-07 21:56:42 +0600

    sprint 2

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,149 @@
+@extends('frontend.frontend_dashboard')
+@section('main')
+
+<!--Page Title-->
+<section class="page-title centred" >
+            <div class="auto-container">
+                <div class="content-box clearfix">
+                    <h1>User Profile </h1>
+                    <ul class="bread-crumb clearfix">
+                        
+                    </ul>
+                </div>
+            </div> 
+        </section>
+        <!--End Page Title-->
+
+
+        <!-- sidebar-page-container -->
+        <section class="sidebar-page-container blog-details sec-pad-2">
+            <div class="auto-container">
+                <div class="row clearfix">
+
+
+
+            @php
+
+                $id = Auth::user()->id;
+                $userData = App\Models\User::find($id);
+
+            @endphp
+
+
+        <div class="col-lg-4 col-md-12 col-sm-12 sidebar-side">
+            <div class="blog-sidebar">
+              <div class="sidebar-widget post-widget">
+                    <div class="widget-title">
+                        <h4>Profile details</h4>
+                    </div>
+                    <div class="post-inner">
+                        <div class="post">
+                            <figure class="post-thumb"><a href="blog-details.html">
+       <img src="assets/images/news/post-1.jpg" alt=""></a></figure>
+        <h5><a href="blog-details.html">{{ $userData-> name}} </a></h5>
+         <p>{{$userData->email}} </p>
+                        </div> 
+                    </div>
+                </div> 
+       
+        <div class="sidebar-widget category-widget">
+            <div class="widget-title">
+                
+            </div>
+            @include('frontend.dashboard.dashboard_sidebar')
+                         
+                        </div>
+                    </div>
+
+
+
+
+  <div class="col-lg-8 col-md-12 col-sm-12 content-side">
+                        <div class="blog-details-content">
+                            <div class="news-block-one">
+                                <div class="inner-box">
+                                    
+                                    <div class="lower-content">
+                                        <h3>Welcome to Your Dashboard</h3>
+                                        <ul class="post-info clearfix">
+                                            <li class="author-box">
+                                                <figure class="author-thumb"><img src="assets/images/news/author-1.jpg" alt=""></figure>
+                                                <h5><a href="blog-details.html">{{ $userData-> name}}</a></h5>
+                                            </li>
+                                        </ul>
+                                      
+          
+
+<div class="row">
+<div class="col-lg-4">
+    <div class="card-body" style="background-color: #1baf65;">
+    <h1 class="card-title" style="color: white; font-weight: bold;">0</h1>
+    <h5 class="card-text"style="color: white;"> Approved properties</h5>
+    
+  </div>
+</div>
+
+<div class="col-md-4">
+    <div class="card-body" style="background-color: #ffc107;">
+    <h1 class="card-title" style="color: white; font-weight: bold; ">0</h1>
+    <h5 class="card-text"style="color: white;"> Pending approve properties</h5>
+    
+  </div>
+</div>
+
+
+<div class="col-md-4">
+    <div class="card-body" style="background-color: #002758;">
+    <h1 class="card-title" style="color: white; font-weight: bold;">0</h1>
+    <h5 class="card-text"style="color: white; "> Rejected properties</h5>
+    
+  </div>
+</div>
+    
+</div> 
+
+                                    </div>
+                                </div>
+                            </div>
+                             
+                            
+                        </div>
+
+    <div class="blog-details-content">
+                            <div class="news-block-one">
+                                <div class="inner-box">
+                                    
+                                    <div class="lower-content">
+                                        <h3>Activity Logs</h3>
+                                      
+                                    </div>
+                                </div>
+                            </div>
+                             
+                            
+                        </div>
+
+
+                    </div> 
+
+
+                </div>
+            </div>
+        </section>
+        <!-- sidebar-page-container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
---
+++ resources/views/frontend/home/header.blade.php
@@ -0,0 +1,99 @@
+<header class="main-header">
+            <!-- header-top -->
+            <div class="header-top">
+                <div class="top-inner clearfix">
+                    <div class="left-column pull-left">
+                        <ul class="info clearfix">
+                            <li><i class="far fa-map-marker-alt"></i>Discover St, Dhaka, Bangladesh</li>
+                            <li><i class="far fa-clock"></i>Mon - Sat  9.00 - 18.00</li>
+                            <li><i class="far fa-phone"></i><a href="tel:0100002023"></a>01000002023</li>
+                        </ul>
+                    </div>
+                    <div class="right-column pull-right">
+                        <ul class="social-links clearfix">
+                            <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-pinterest-p"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-vimeo-v"></i></a></li>
+                        </ul>
+        @auth
+         <div class="sign-box">
+                <a href="{{ route('dashboard') }}"><i class="fas fa-user"></i>Dashboard</a>
+               <a href="{{ route('user.logout') }}"><i class="fas fa-user"></i>Logout</a>
+        </div> 
+         @else 
+         <div class="sign-box">
+         <a href="{{ route('login') }}"><i class="fas fa-user"></i>Sign In</a>
+                        </div>
+         @endauth
+                    </div>
+                </div>
+            </div>
+<!-- header-lower -->
+<div class="header-lower">
+<div class="outer-box">
+<div class="main-box">
+<div class="logo-box">
+    <figure class="logo"><a href="index.html"><img src="{{ asset('frontend/assets/images/real-estate-logo.png') }}" alt=""></a></figure>
+</div>
+<div class="menu-area clearfix">
+    <!--Mobile Navigation Toggler-->
+    <div class="mobile-nav-toggler">
+        <i class="icon-bar"></i>
+        <i class="icon-bar"></i>
+        <i class="icon-bar"></i>
+    </div>
+    <nav class="main-menu navbar-expand-md navbar-light">
+        <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+            <ul class="navigation clearfix">
+                <li ><a href="{{route('home')}}"><span>Main Home</span></a></li> 
+
+                <li class="dropdown"><a href="{{route('home')}}"><span>Property</span></a>
+                    <ul>
+                        <li><a href="{{route('buy.property')}}">Buy Property </a></li>
+                        <li><a href="{{route('rent.property')}}">Rent Property </a></li>
+                    </ul>
+                </li>
+
+
+                <li class="dropdown"><a href="index.html"><span>Agency</span></a>
+                    <ul>
+                        <li><a href="agency-list.html">Agent List</a></li>
+                    </ul>
+                </li>
+
+
+                <li ><a href="{{route('home')}}"><span>About Us</span></a></li>
+
+                <li><a href="{{route('home')}}"><span>Contact</span></a></li>   
+            </ul>
+        </div>
+    </nav>
+</div>
+<div class="btn-box">
+    <a href="index.html" class="theme-btn btn-one"><span>+</span>Add Listing</a>
+</div>
+</div>
+</div>
+</div>
+
+            <!--sticky Header-->
+            <div class="sticky-header">
+                <div class="outer-box">
+                    <div class="main-box">
+                        <div class="logo-box">
+                            <figure class="logo"><a href="index.html"><img src="{{ asset('frontend/assets/images/real-estate-logo.png') }}" alt=""></a></figure>
+                        </div>
+                        <div class="menu-area clearfix">
+                            <nav class="main-menu clearfix">
+                                <!--Keep This Empty / Menu will come through Javascript-->
+                            </nav>
+                        </div>
+                        <div class="btn-box">
+                            <a href="index.html" class="theme-btn btn-one"><span>+</span>Add Listing</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
